dbef143f27de6d87d748f421130dfc1a76a9d97c
Koichi Shiraishi <zchee.io@gmail.com>
2021-08-24 04:17:07 +0900

    pkg/cmd: add cmd package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ pkg/cmd/cmd.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The protobuf-tools Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+// AppName is the protoc-gen-kube binary name.
+const AppName = "protoc-gen-kube"
